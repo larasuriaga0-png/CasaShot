e0521e1b12e42253ff53f8ee1548ccef63f89a05
larasuriaga0-png <larasuriaga0@gmail.com>
2026-09-15 15:59:34 +0800

    Initial commit

---
+++ src/Home.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import "./Home.css";
+
+import backgroundImage from "./assets/event_background.jpg";
+import foodImage from "./assets/event_food.jpg";
+import logoImage from "./assets/logo.jpg";
+
+function Home() {
+  return (
+    <div className="home">
+      {/* mavbar */}
+      <nav className="navbar">
+        <div className="nav-left">
+          <a href="#home">Home</a>
+          <a href="#about">About Us</a>
+          <a href="#faq">FAQ</a>
+        </div>
+
+        {/* logo */}
+        <div className="logo">
+          <img src={logoImage} alt="CaterShot Logo" />
+        </div>
+
+        <div className="nav-right">
+          <a href="#testimonials">Testimonials</a>
+          <a href="#feedbacks">Feedbacks</a>
+          <a href="#login">Login/Signup</a>
+        </div>
+      </nav>
+
+      {/* main */}
+      <section
+        className="hero"
+        id="home"
+        style={{
+          backgroundImage: `url(${backgroundImage})`,
+        }}
+      >
+        <div className="overlay"></div>
+
+        <div className="hero-content">
+          <h1>Cater Shot</h1>
+
+          <p>
+            Creating beautifully curated celebrations with exquisite details,
+            <br />
+            delicious moments, and memories you'll cherish forever.
+          </p>
+
+          <button className="start-button">Start Planning</button>
+        </div>
+
+        {/* image sa gilid */}
+        <div className="food-image">
+          <img src={foodImage} alt="Beautiful event setup" />
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Home;
